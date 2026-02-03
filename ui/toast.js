7c4ad6a208725f80ca1b56d97e2c03290ccbf2d5
Byun Huỳnh <byun.huynh@gmail.com>
2026-02-03 20:31:41 +0700

    cập nhật toast thêm màu

--- ui/toast.js
+++ ui/toast.js
@@ -5,9 +5,10 @@ function isMobile() {
 
 function showToast(text, type = "info") {
   const colors = {
-    success: "linear-gradient(to right, #22c55e, #16a34a)",
-    error: "linear-gradient(to right, #ef4444, #dc2626)",
-    info: "linear-gradient(to right, #3b82f6, #2563eb)",
+    success: "linear-gradient(to right, #22c55e, #16a34a)", // xanh
+    error: "linear-gradient(to right, #ef4444, #dc2626)", // đỏ
+    info: "linear-gradient(to right, #3b82f6, #2563eb)", // xanh dương
+    warning: "linear-gradient(to right, #facc15, #eab308)", // 🟡 vàng
   };
 
   Toastify({
@@ -15,8 +16,8 @@ function showToast(text, type = "info") {
     duration: 3000,
     gravity: isMobile() ? "bottom" : "top",
     position: isMobile() ? "center" : "right",
-    backgroundColor: colors[type] || colors.info,
     style: {
+      backgroundColor: colors[type] || colors.info,
       borderRadius: "12px",
       fontSize: "14px",
       maxWidth: "90%",
