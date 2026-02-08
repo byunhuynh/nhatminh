1fd0abf2ad2635e28c878f0e6df8a13eb94ac7b1
byunhuynh <byun.huynh@gmail.com>
2026-02-08 09:18:07 +0700

    Update config.js
    đổi config

--- core/config.js
+++ core/config.js
@@ -1,7 +1,7 @@
 // js/core/config.js
 
-//const API = "https://backendnhatminh-ioim.onrender.com";
-
-const API = "http://127.0.0.1:8000";
+//const API = "https://backendnhatminh-ioim.onrender.com"; //server
+//const API = "http://127.0.0.1:8000"; //local
+const API = "https://backendnhatminh-ioim.onrender.com"
 // ✅ FIX: thêm /v1
 const API_PROVINCE = "https://provinces.open-api.vn";
